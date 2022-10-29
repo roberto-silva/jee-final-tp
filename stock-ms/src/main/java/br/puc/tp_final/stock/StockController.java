96818d856afdca2bb73d4538c560a124d23ee917
elton.jones <elton.jones@ivoryit.com.br>
2022-10-29 17:42:46 -0300

    Implementação de estoque em andamento

--- stock-ms/src/main/java/br/puc/tp_final/stock/StockController.java
+++ stock-ms/src/main/java/br/puc/tp_final/stock/StockController.java
@@ -2,6 +2,9 @@ package br.puc.tp_final.stock;
 
 import jakarta.ejb.EJB;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.xml.ws.http.HTTPException;
 
 //http://localhost:8080/stock-ms/rest/stock/status/1
 
@@ -12,24 +15,42 @@ public class StockController {
     @EJB
     private StockService stockService;
 
+
     @POST
-    @Path("write_off")
-    @Consumes("application/json")
-    public void pay() {
-        stockService.write_off();
+    @Path("/write-off/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response downStock()
+    {
+        try
+        {
+            stockService.write_off(1);
+            return  Response.status(Response.Status.OK).build();
+        }
+        catch (HTTPException e)
+        {
+            //todo: add log para monitoramento
+            throw e;
+        }
     }
+
+
+
 
     @GET
     @Path("/status/{id}")
     @Consumes("application/json")
-    public String status() {
-        return stockService.status();
+    public String status()
+    {
+        return  "tsteste";
+        //return stockService.status();
     }
+
 
     @GET
     @Path("/teste-probabilidade-baixa-estoque")
     @Consumes("application/json")
     public boolean TestePedidoAceito() {
-        return stockService.SimulaProbabilidadeBaixaEstoque();
+        return stockService.simulaProbabilidadeBaixaEstoque();
     }
 }
--- stock-ms/src/main/java/br/puc/tp_final/stock/StockService.java
+++ stock-ms/src/main/java/br/puc/tp_final/stock/StockService.java
@@ -1,6 +1,8 @@
 package br.puc.tp_final.stock;
 
 import jakarta.ejb.Stateless;
+import jakarta.ws.rs.core.Response;
+import jakarta.xml.ws.http.HTTPException;
 
 import java.util.Random;
 
@@ -8,16 +10,25 @@ import java.util.Random;
 public class StockService {
 
 
-    public void write_off() {
+    public void write_off(int id)
+    {
+        boolean itemBaixadoComSucesso = simulaProbabilidadeBaixaEstoque();
+
+        if(!itemBaixadoComSucesso)
+        {
+            throw new HTTPException(Response.Status.CONFLICT.getStatusCode());
+        }
     }
 
     public String status() {
         return "TODO";
     }
 
-    public boolean SimulaProbabilidadeBaixaEstoque(){
+    public boolean simulaProbabilidadeBaixaEstoque(){
         Random random = new Random();
         int probabilidade = random.nextInt(100);
         return probabilidade < 95;
     }
+
+
 }
